bfa2cddfbba86f03b5ca1391b5c66b63f00e1602
kfgenius@naver.com <kfgenius@naver.com@48db4b40-d568-11de-ba7e-09ca73f1295e>
2010-12-11 07:33:31 +0000

    이벤트 ID 변경 다이얼로그 제작 저장하지 않고 New나 Open을 할 경우 저장할 건지 묻는 창이 뜸

--- MapEditor/resource2.h
+++ MapEditor/resource2.h
@@ -6,6 +6,7 @@
 #define IDD_UNITATTR                    102
 #define IDD_NEW_MAP                     104
 #define IDD_OPEN_MAP                    105
+#define IDD_EVENT_ID                    106
 #define IDC_TILENO                      1001
 #define IDC_LAYER                       1002
 #define IDC_MOVE                        1003
@@ -30,14 +31,15 @@
 #define IDC_LIST1                       1020
 #define IDC_MAP_LIST                    1020
 #define IDC_FILENAME                    1021
+#define IDC_EVENT_ID                    1021
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40006
-#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_CONTROL_VALUE         1022
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
